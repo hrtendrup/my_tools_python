959b55c2ce8c0759a97d60ef52be511e2c56342c
Hans Tendrup <hans.tendrup@foredtert.com>
2022-04-14 14:50:26 -0500

    updated error messaging

--- ip2hex.py
+++ ip2hex.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-def ip2hex(ip):
+def ip2hex(iporhex):
     '''
     '''
     import ipaddress
     try:
       try:
-        return hex(int(ipaddress.ip_address(ip)))
+        return hex(int(ipaddress.ip_address(iporhex)))
       except:
-        return str(ipaddress.ip_address(int(ip,16)))
+        return str(ipaddress.ip_address(int(iporhex,16)))
     except:
         raise ValueError("Wrong format")
 
@@ -16,5 +16,9 @@ hex2ip = ip2hex
 
 if __name__ == "__main__":
     import sys
-    arg = sys.argv[1]
-    print(ip2hex(arg))
+    try:
+        arg = sys.argv[1]
+    except IndexError:
+        print("Error: Missing IP or HEX value")
+    else:
+        print(ip2hex(arg))
